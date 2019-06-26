06fa6f1d248de9a7a5a05442ef1003d63565b52b
tetradog <tetradog@gmail.com>
2019-06-26 13:21:29 +0200

    añadiendo test

---
+++ TestGabrielCatSSeguretat/CrazyKeyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gabriel.Cat.S.Seguretat;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestGabrielCatSSeguretat
+{
+    [TestClass]
+    public class CrazyKeyTest
+    {
+        [TestMethod]
+        public void GenerarDosVecesLaMismaClaveCorrectamente()
+        {
+            Key key1, key2, keyOrigen;
+            CrazyKey cKey=new CrazyKey(5);
+
+            keyOrigen = Key.GetKey(10);
+            key1 = cKey.Encrypt(keyOrigen);
+            key2 = cKey.Encrypt(keyOrigen);
+
+            Assert.IsTrue(key1.ItemsKey.Count == key2.ItemsKey.Count && key1.Equals(key2));
+            
+
+        }
+    }
+}
